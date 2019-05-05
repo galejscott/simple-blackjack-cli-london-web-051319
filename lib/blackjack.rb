a370e26aaca2f4f84409912c5341ecb611f82e73
Scott Gale <galejscott@gmail.com>
2019-05-05 14:37:32 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -44,7 +44,8 @@ end
 def invalid_command
   # code invalid_command here
   puts "Please enter a valid command"
-  
+  prompt_user
+  get_user_input
 end
 
 #####################################################
